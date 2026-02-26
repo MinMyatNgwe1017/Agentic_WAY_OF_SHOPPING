fd0400506b141ffcf6f9b97d2b7f191153ad2960
Isra <israelibrahim20@gmail.com>
2026-02-26 11:13:00 +0200

    Added login page and some initial login backend logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { testConnection } from './services/api'
-
+import Login from './components/Login'
 function App() {
-  const [count, setCount] = useState(0)
 
-  const handleTestConnection = async () => {
-    try {
-      const response = await testConnection();
-      alert(`API Response: ${response.message}`);
-    } catch (error) {
-      console.error('Error testing API connection:', error);
-      alert('Failed to connect to the API. Check the console for details.');
-    }
-  }
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Login />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <button onClick={handleTestConnection}>
-          Test API Connection </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -14,3 +14,9 @@ export const testConnection = async () => {
     const response = await api.get('/test');
     return response.data;
 }
+
+export const login = async (loginData: { email: string; password: string }) => {
+    const response = await api.post('/auth/login', loginData);
+    console.log('Login response:', response.data);
+    return response.data;
+}
